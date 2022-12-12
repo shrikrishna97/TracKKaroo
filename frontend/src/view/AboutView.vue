5f393035bcda3bcc5fa14a5696145e0ff8869325
Shri Krishna Pandey <96811526+shrikrishna97@users.noreply.github.com>
2022-12-13 02:22:43 +0530

    Add files via upload

---
+++ frontend/src/view/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an about page. Is it working</h1>
+  </div>
+</template>
---
+++ frontend/src/view/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img alt="TracK Karoo logo" src="../assets/logo.jpg" />
+    <HelloWorld msg="Welcome to TracK Karoo App" />
+        <div class="container-fluid">
+    <p style="text-align:center">
+      <h4 style="text-align: center;color:brown;font-size:large;font-family:Verdana;"> 
+        The quantified self (QS) is, any individual engaged in the self-tracking of any kind of biological, physical, behavioral, or environmental information. 
+                    There is a proactive stance toward obtaining information and acting on it. A variety of areas may be tracked and analyzed, for example, weight, energy level, mood, time usage, sleep quality, health, cognitive performance, athletics, and learning strategies. 
+                    Health is an important but not exclusive focus, where objectives may range from general tracking to pathology resolution to physical and mental performance enhancement.</h4>
+                    <h3 style="font-size: medium;font-style: oblique; color:black;">So, this is a small help from my end to your betterment.Use this webapp to <span style="color:blue">tracker your habbits</span>.</h3>
+            </p>
+                
+                </div>
+                
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+// import login from "@/components/login.vue";
+// import register from "@/components/registration.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
